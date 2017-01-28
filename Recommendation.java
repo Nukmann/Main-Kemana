49194eeb9c1bd54f26f128cc760299ac9eaac314
nukimen <nukman.mhmmd@gmail.com>
2017-01-28 16:20:53 +0700

    change public class

--- Recommendation.java
+++ Recommendation.java
@@ -1,7 +1,6 @@
 package app.travelling.srw;
 
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
